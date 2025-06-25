38060701ac889be38552e37cc4eac130c215d3c0
BigSlatt998 <ganiyutaofeeqolarewaju@gmail.com>
2025-06-25 19:06:03 +0100

    Deployed websit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
    server:{
     host: true,
+    headers: {
+      "content-type": "text/javascript",
+    }
   },
    build: {
     assetsDir: 'assets',
